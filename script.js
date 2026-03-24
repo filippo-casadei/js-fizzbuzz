46b8dc4c33503ab47119c0394afeb637355afe8d
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-24 14:31:19 +0100

    Riportata consegna dell'esercizio.

---
+++ script.js
@@ -0,0 +1,5 @@
+// CONSEGNA: 
+// Creare un programma che stampi i numeri da 1 a 100.
+// Per i numeri multipli di 3 stampi "Fizz"
+// Per i multipli numeri di 5 stampi "Buzz"
+// Per i numeri multipli di 3 e 5 stampi "FizzBuzz"
