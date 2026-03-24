871f3986cc5411625f04f91e60d174d8569cee1c
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-24 15:05:16 +0100

    Impostando il ragionamento pt2.

--- script.js
+++ script.js
@@ -10,3 +10,5 @@
 // - Sicuramente devo utilizzare il ciclo FOR per fare in modo che il programma mi stampi i 
 // numeri da 1 a 100. 
 // - Dovremmo utilizzare % 3 e % 5 e se il risultato sarà 0 vorrà dire che sarà un multiplo.
+// - Nelle condizioni il programma dovrà analizzare prima 3 && 5 insieme altrimenti arrivati
+// a 15 lo vedrebbe solo come multiplo di 3 o di 5.
