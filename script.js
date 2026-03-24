c9f789335f879e5daba746d4f08913b1e85707fe
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-24 15:02:33 +0100

    Impostando il ragionamento.

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 // Per i numeri multipli di 3 stampi "Fizz"
 // Per i multipli numeri di 5 stampi "Buzz"
 // Per i numeri multipli di 3 e 5 stampi "FizzBuzz"
+
+// ====================================================================================
+
+// Ragionamento: 
+// - Sicuramente devo utilizzare il ciclo FOR per fare in modo che il programma mi stampi i 
+// numeri da 1 a 100. 
+// - Dovremmo utilizzare % 3 e % 5 e se il risultato sarà 0 vorrà dire che sarà un multiplo.
