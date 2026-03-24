c63738087834e66979437a2cadda49553cde50c8
Filippo Casadei <casadeifilippo91@gmail.com>
2026-03-24 15:11:13 +0100

    Impostando il ragionamento pt3

--- script.js
+++ script.js
@@ -8,7 +8,13 @@
 
 // Ragionamento: 
 // - Sicuramente devo utilizzare il ciclo FOR per fare in modo che il programma mi stampi i 
-// numeri da 1 a 100. 
+// numeri da 1 a 100.
+
 // - Dovremmo utilizzare % 3 e % 5 e se il risultato sarà 0 vorrà dire che sarà un multiplo.
+
 // - Nelle condizioni il programma dovrà analizzare prima 3 && 5 insieme altrimenti arrivati
 // a 15 lo vedrebbe solo come multiplo di 3 o di 5.
+
+// - Con console.log chiediamo, all'interno di un IF(multiplo di 3e5) + ELSE IF(multiplo
+// di 3) + ELSE IF(multiplo di 5) + ELSE(tutti gli altri numeri) di stampare il risultato
+// inserendo Fizz, Buzz o FizzBuzz.
